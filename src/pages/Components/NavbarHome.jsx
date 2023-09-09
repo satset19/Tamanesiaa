5c99e3f635291f18246660df66f283cc2df05439
satset19 <laksonosatiyo@gmail.com>
2023-09-09 23:31:15 +0700

    nav

--- src/pages/Components/NavbarHome.jsx
+++ src/pages/Components/NavbarHome.jsx
@@ -1,92 +1,98 @@
 import React, { useState, useEffect } from "react";
-import { debounce } from "../../utilities/helpers";
+import { useScroll } from "../../utilities/helpers";
 import { Button, Navbar } from "flowbite-react";
 
 const NavbarHome = () => {
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [visible, setVisible] = useState(true);
-
-  const handleScroll = debounce(() => {
-    const currentScrollPos = window.pageYOffset;
-
-    setVisible(
-      (prevScrollPos > currentScrollPos &&
-        prevScrollPos - currentScrollPos > 70) ||
-        currentScrollPos < 10
-    );
-
-    setPrevScrollPos(currentScrollPos);
-  }, 100);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos, visible, handleScroll]);
-
-  const navbarStyles = {
-    position: "fixed",
-    transition: "top 0.3s",
+  const { scrollY, x, scrollDirection } = useScroll();
+  const styles = {
+    active: {
+      visibility: "visible",
+      transition: "all 0.5s",
+    },
+    hidden: {
+      visibility: "active",
+      transition: "all 0.5s",
+      transform: "translateY(-50%)",
+    },
   };
 
+  const setStyle = () => {
+    return scrollDirection === "down" ? styles.hidden : styles.active;
+  };
+
+  useEffect(() => {
+    setStyle();
+  }, [scrollY]);
   return (
-    <header
-      class="bg-white top-0 w-full shadow-md py-3"
-      style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}
-    >
-      <nav class="container mx-auto px-6 py-3">
-        <div class="md:hidden flex items-center">
-          <button
-            class="text-gray-800 focus:outline-none mt-3 left-5"
-            style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}
-          >
-            <svg
-              class="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div class="flex justify-center items-center">
-          <a
-            href="#"
-            class="text-2xl font-bold text-gray-800 top-2 mt-2 left-14"
-            style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}
-          >
+    <header className="bg-white top-0 w-full shadow-md py-4" style={setStyle()}>
+      <nav className="container mx-auto px-6">
+        <button className="md:hidden text-gray-800 focus:outline-none absolute left-5 top-5">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+        <div className="md:hidden flex justify-center">
+          <a href="#" className="text-2xl font-bold text-gray-800 left-0">
             Tamanesia
           </a>
-          <div
-            class="hidden md:flex items-center space-x-4 mt-3"
-            style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}
-          >
-            <a href="#" class="text-gray-800 hover:text-blue-600">
+        </div>
+        <div className="flex justify-center items-center relative max-md:hidden">
+          <a
+            href="#"
+            className="text-2xl font-bold text-gray-800  absolute left-0">
+            Tamanesia
+          </a>
+          <div className="hidden md:flex items-center space-x-4 ">
+            <a href="#" className="text-gray-800 hover:text-blue-600">
               Home
             </a>
-            <a href="#" class="text-gray-800 hover:text-blue-600">
+            <a href="#" className="text-gray-800 hover:text-blue-600">
               About
             </a>
-            <a href="#" class="text-gray-800 hover:text-blue-600">
+            <a href="#" className="text-gray-800 hover:text-blue-600">
               Services
             </a>
-            <a href="#" class="text-gray-800 hover:text-blue-600">
+            <a href="#" className="text-gray-800 hover:text-blue-600">
               Contact
             </a>
-            {/* <a href="#" class="bg-blue-600 text-white px-4 py-2 rounded-md">
+            {/* <a href="#" className="bg-blue-600 text-white px-4 py-2 rounded-md">
               Sign Up
             </a> */}
           </div>
         </div>
       </nav>
     </header>
+
+    // <nav
+    //   className="Header"
+    //   style={scrollDirection === "down" ? styles.active : styles.hidden}>
+    //   <div className="py-5 bg-white"></div>
+    //   {/* <Link to="/" className="Header__link">
+    //     <img src={Logo} height="50px" width="auto" alt="logo" />
+    //     <div className="Header__link__title">Chronology</div>
+    //   </Link>
+    //   <ul className="flex">
+    //     <li>
+    //       <Link to="/about" className="Header__link">
+    //         About
+    //       </Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/blog" className="Header__link">
+    //         Blogs
+    //       </Link>
+    //     </li>
+    //   </ul> */}
+    // </nav>
   );
 };
 
--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,15 +1,37 @@
-export function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+
+import { useState, useEffect } from "react";
+
+export function useScroll() {
+  // storing this to get the scroll direction
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  // the offset of the document.body
+  const [bodyOffset, setBodyOffset] = useState(
+    document.body.getBoundingClientRect()
+  );
+  // the vertical direction
+  const [scrollY, setScrollY] = useState(bodyOffset.top);
+  // scroll direction would be either up or down
+  const [scrollDirection, setScrollDirection] = useState();
+
+  const listener = e => {
+    setBodyOffset(document.body.getBoundingClientRect());
+    setScrollY(bodyOffset.top);
+    setLastScrollTop(bodyOffset.top);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+  });
+
+  useEffect(() => {
+    setScrollDirection(scrollY >= -160 ? "up" : "down");
+  }, [scrollY])
+
+  return {
+    scrollY,
+    scrollDirection
   };
 }
